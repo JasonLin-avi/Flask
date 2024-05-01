b27f84b47ed5bdeaccdc4792b97375a63776ed33
Jasonavi <avilin@gmail.com>
2024-05-01 21:25:35 +0800

    artillery add header example and flask get header

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, make_response
+from flask import request
+from logging import Logger
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # 取得特定 header 的值
+    header_value = request.headers.get('x-jwt')
+    print(header_value)
+    if header_value != 'Jason':
+        app.logger.warning('not accept token')
+        return make_response('Hello, world! 123',403)
+    
     response = make_response('Hello, world! 123')
     response.set_cookie('token','token value')
     
